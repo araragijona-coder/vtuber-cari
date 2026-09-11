2ea6b353914f0865e0da95b52876ae3d4f4cd02a
araragijona-coder <araragijona@gmail.com>
2026-09-11 04:14:16 -0300

    feat: expose production Twitch runtime

--- app/twitch/__init__.py
+++ app/twitch/__init__.py
@@ -1,5 +1,6 @@
 from .adapter import TwitchAdapter
 from .client import TwitchConfig
+from .live import TwitchLiveBot
 from .models import ChatMessage
 
-__all__ = ["ChatMessage", "TwitchAdapter", "TwitchConfig"]
+__all__ = ["ChatMessage", "TwitchAdapter", "TwitchConfig", "TwitchLiveBot"]
