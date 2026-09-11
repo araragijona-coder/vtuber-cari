c210e2536d30fee3c89cfdf5bb23a1f1358e01f7
araragijona-coder <araragijona@gmail.com>
2026-09-11 14:59:29 -0300

    chore: package experimental avatar contracts

---
+++ experimental/avatar/__init__.py
@@ -0,0 +1,6 @@
+"""Experimental Cari avatar contracts and renderer-independent tests."""
+
+from .acting_state import AvatarActingState
+from .renderer_protocol import AvatarRenderer
+
+__all__ = ["AvatarActingState", "AvatarRenderer"]
