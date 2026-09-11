87e9059f29cb6c5eaadb15e7c5dcd865667e0597
araragijona-coder <araragijona@gmail.com>
2026-09-11 02:31:50 -0300

    Pass 24: expose persistent memory store

--- app/memory/__init__.py
+++ app/memory/__init__.py
@@ -1,3 +1,4 @@
+from .persistent import PersistentMemoryStore
 from .session import MemoryItem, SessionMemory, SessionTurn
 
-__all__ = ["MemoryItem", "SessionMemory", "SessionTurn"]
+__all__ = ["MemoryItem", "PersistentMemoryStore", "SessionMemory", "SessionTurn"]
