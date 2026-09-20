f1c3acb5324ac39ba281520083c700aceb3012b5
araragijona-coder <araragijona@gmail.com>
2026-09-20 14:28:26 -0300

    feat(avatar): add sandboxed overlay preload

---
+++ experimental/studio/electron-shell/avatar-overlay-preload.js
@@ -0,0 +1,10 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("cariAvatar", {
+  config: () => ipcRenderer.invoke("avatar:config"),
+  onState: callback => {
+    const listener = (_, state) => callback(state);
+    ipcRenderer.on("avatar:state", listener);
+    return () => ipcRenderer.removeListener("avatar:state", listener);
+  }
+});
