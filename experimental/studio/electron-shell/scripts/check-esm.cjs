c52913fcd6e65f7bdd2e1d180cfb09b77338623a
araragijona-coder <araragijona@gmail.com>
2026-09-21 04:28:12 -0300

    test(vtuber): include tracking profile in ESM check

--- experimental/studio/electron-shell/scripts/check-esm.cjs
+++ experimental/studio/electron-shell/scripts/check-esm.cjs
@@ -11,6 +11,7 @@ const files = [
   "avatar/acting-bridge.js",
   "avatar/face-tracker.js",
   "avatar/face-tracking-bridge.js",
+  "avatar/tracking-profile.js",
   "avatar/three-avatar.js",
   "avatar/action-store.js",
   "avatar/speech-activity.js",
