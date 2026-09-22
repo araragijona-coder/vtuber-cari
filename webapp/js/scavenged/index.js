e324f8df357d11dd9bebd9192ba30b52a226852c
araragijona-coder <araragijona@gmail.com>
2026-09-22 02:38:14 -0300

    feat(scavenger): add index.js

---
+++ webapp/js/scavenged/index.js
@@ -0,0 +1,4 @@
+export { CombatEngine } from "./combat-engine.js";
+export { selectTarget, bindCanvasTargetSelection } from "./target-selector.js";
+export { ImpactParticlePool } from "./impact-particles.js";
+export { ArcadeLoop } from "./arcade-loop.js";
